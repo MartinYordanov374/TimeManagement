267e021a752dc32447bbd7206068c9b93573b3a9
GoofyGalaxy374 <prophetyordanov@gmail.com>
2021-12-06 16:37:53 +0200

    Updated the databaseScript. The allProjects table no longer contains the projectCompleted row. Removed the unique constraint from the projectName row

--- MaterialDesign/bin/Debug/backend/databaseScript.py
+++ MaterialDesign/bin/Debug/backend/databaseScript.py
@@ -5,34 +5,42 @@ weekDays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", 
 
 targetDatabase = sqlite3.connect('progressDatabase.db')
 
+def createTables():
+    # creating the progress and allProjects tables
+    createTableString = '''CREATE TABLE IF NOT EXISTS progress (
+    weekday VARCHAR(255) NOT NULL UNIQUE PRIMARY KEY, 
+    projectsCompleted integer NOT NULL)'''
 
-# creating the progress and allProjects tables
-createTableString = '''CREATE TABLE IF NOT EXISTS progress (
-weekday VARCHAR(255) NOT NULL UNIQUE PRIMARY KEY, 
-projectsCompleted integer NOT NULL)'''
-
-targetDatabase.execute(createTableString)
-
-
-createAllProjectsTableString = '''CREATE TABLE IF NOT EXISTS allProjects (
-projectName VARCHAR(255) NOT NULL UNIQUE PRIMARY KEY, 
-projectCompleted integer NOT NULL)'''
-
-targetDatabase.execute(createAllProjectsTableString)
+    targetDatabase.execute(createTableString)
 
 
-for i in range(0,7):
-    val = (weekDays[i], 0)
-    targetDatabase.execute('INSERT OR IGNORE INTO progress (weekday, projectsCompleted) VALUES(?,?)', val)
+    createAllProjectsTableString = '''CREATE TABLE IF NOT EXISTS allProjects (
+    projectName VARCHAR(255) NOT NULL PRIMARY KEY)'''
+
+    targetDatabase.execute(createAllProjectsTableString)
+    
+
+def populateTables():
+    for i in range(0,7):
+        val = (weekDays[i], 0)
+        targetDatabase.execute('INSERT OR IGNORE INTO progress (weekday, projectsCompleted) VALUES(?,?)', val)
+        targetDatabase.commit()
+
+    selectString = "SELECT * FROM progress"
     targetDatabase.commit()
+    result = targetDatabase.execute(selectString)
 
-selectString = "SELECT * FROM progress"
-targetDatabase.commit()
-result = targetDatabase.execute(selectString)
+    timeSpentOnProjectsY = []
+    for row in result:
+        weekday = row[0]
+        projectsCompleted = row[1]
+        timeSpentOnProjectsY.append(float(projectsCompleted))
+    
 
-timeSpentOnProjectsY = []
-for row in result:
-    weekday = row[0]
-    projectsCompleted = row[1]
-    timeSpentOnProjectsY.append(float(projectsCompleted))
+createTables()
+
+populateTables()
+
+
+
 
